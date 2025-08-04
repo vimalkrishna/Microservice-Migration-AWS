c641320af700bffd8e88282e9e21ae86e172e61d
Vimal Krishna <krishnavimal@gmail.com>
2025-08-04 15:46:10 +0200

    Separated CDK and Microservice in their folders

---
+++ microservices/frontend-visualization-app/src/main/java/com/mylife/ZustellerApplication.java
@@ -0,0 +1,13 @@
+package com.mylife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZustellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZustellerApplication.class, args);
+	}
+}
+ 
